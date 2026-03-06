6d64379e4cdffc60282551c7488cd99419dc1255
ELB HAKIM <hakim@MacBook-Air-de-ELB.local>
2026-03-06 02:01:36 +0100

    connect frontend to backend + env apiUrl

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { POSTS, Post } from '../data/post';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../data/post';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
   getPosts(): Observable<Post[]> {
-    return of(POSTS);
+    return this.http.get<Post[]>(`${this.apiUrl}/v1/posts`);
   }
 }
